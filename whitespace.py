bb8ae6d6de8051d1becd7a66b7fd77f0d76ff220
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-21 18:37:56 +0100

    Removing whitespace

--- whitespace.py
+++ whitespace.py
@@ -1,3 +1,3 @@
-favorite_language = 'python '
-favorite_language = favorite_language.rstrip()
+favorite_language = ' python '
+favorite_language = favorite_language.strip()
 print(favorite_language)
